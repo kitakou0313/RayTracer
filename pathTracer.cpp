d8f108724e31297d6c0d21cb2a638e8a2d8375f5
kitakou0313 <kitakou0313@gmail.com>
2020-02-28 15:10:35 +0900

    Add ray Class

--- pathTracer.cpp
+++ pathTracer.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 #include <bits/stdc++.h>
-#include "vec3.h"
+#include "ray.h"
 
 using namespace std;
+
+vec3 color(const ray &r)
+{
+    vec3 unitDirection = unitVector(r.direction());
+    float t = 0.5 * (unitDirection.y() + 1.0);
+    return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+}
 
 int main()
 {
@@ -15,11 +22,23 @@ int main()
     cout << "P3\n"
          << nx << " " << ny << "\n255\n";
 
+    vec3 lowerLeftCorner(-2.0, -1.0, -1.0);
+    vec3 horizontal(4.0, 4.0, 4.0);
+    vec3 vertical(0.0, 2.0, 0.0);
+    vec3 origin(0.0, 0.0, 0.0);
+
     for (int j = ny - 1; j >= 0; j--)
     {
         for (int i = 0; i < nx; i++)
         {
-            vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
+
+            float u = float(i) / float(nx);
+            float v = float(j) / float(ny);
+
+            ray r(origin, lowerLeftCorner + u * horizontal + v * vertical);
+
+            vec3 col = color(r);
+
             int ir = int(255.99 * col[0]);
             int ig = int(255.99 * col[1]);
             int ib = int(255.99 * col[2]);
