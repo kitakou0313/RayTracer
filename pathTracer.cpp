513203418441b6fd61d1e94e0a83dc59b10b282c
kitakou0313 <kitakou0313@gmail.com>
2020-02-28 17:52:58 +0900

    Add hitable and sphere  class

--- pathTracer.cpp
+++ pathTracer.cpp
@@ -1,26 +1,24 @@
 #include <iostream>
 #include <bits/stdc++.h>
-#include "ray.h"
+#include "sphere.h"
+#include "hitablelist.h"
+#include "float.h"
 
 using namespace std;
 
-bool hitSphere(const vec3 &center, float radius, const ray &r)
+vec3 color(const ray &r, hitable *world)
 {
-    vec3 oc = r.origin() - center;
-    float a = dot(r.direction(), r.direction());
-    float b = 2.0 * dot(oc, r.direction());
-    float c = dot(oc, oc) - radius * radius;
-    float discriminant = b * b - 4 * a * c;
-    return (discriminant > 0);
-}
-
-vec3 color(const ray &r)
-{
-    if (hitSphere(vec3(0, 0, -1), 0.5, r))
-        return vec3(1, 0, 0);
-    vec3 unitDirection = unitVector(r.direction());
-    float t = 0.5 * (unitDirection.y() + 1.0);
-    return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+    hitRecord rec;
+    if (world->hit(r, 0.0, FLT_MAX, rec))
+    {
+        return 0.5 * vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
+    }
+    else
+    {
+        vec3 unitDirection = unitVector(r.direction());
+        float t = 0.5 * (unitDirection.y() + 1.0);
+        return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+    }
 }
 
 int main()
@@ -39,6 +37,14 @@ int main()
     vec3 vertical(0.0, 2.0, 0.0);
     vec3 origin(0.0, 0.0, 0.0);
 
+    hitable *list[1];
+
+    list[0] = new sphere(vec3(0, 0, -1), 0.5);
+
+    list[1] = new sphere(vec3(0, -100.5, -1), 100);
+
+    hitable *world = new hitableList(list, 2);
+
     for (int j = ny - 1; j >= 0; j--)
     {
         for (int i = 0; i < nx; i++)
@@ -49,7 +55,9 @@ int main()
 
             ray r(origin, lowerLeftCorner + u * horizontal + v * vertical);
 
-            vec3 col = color(r);
+            vec3 p = r.pointAtParameter(2.0);
+
+            vec3 col = color(r, world);
 
             int ir = int(255.99 * col[0]);
             int ig = int(255.99 * col[1]);
---
+++ sphere.h
@@ -0,0 +1,46 @@
+#ifndef SPHEREH
+#define SPHEREH
+#include "hitable.h"
+
+class sphere : public hitable
+{
+public:
+    sphere() {}
+    sphere(vec3 cen, float r) : center(cen), radius(r){};
+    virtual bool hit(const ray &r, float tMin, float tMax, hitRecord &rec) const;
+    vec3 center;
+    float radius;
+};
+
+bool sphere::hit(const ray &r, float tMin, float tMax, hitRecord &rec) const
+{
+    vec3 oc = r.origin() - center;
+    float a = dot(r.direction(), r.direction());
+    float b = dot(oc, r.direction());
+    float c = dot(oc, oc) - radius * radius;
+    float discriminant = b * b - a * c;
+    if (discriminant > 0)
+    {
+        float temp = (-b - sqrt(b * b - a * c)) / a;
+        if (temp < tMax && temp > tMin)
+        {
+            rec.t = temp;
+            rec.p = r.pointAtParameter(rec.t);
+            rec.normal = (rec.p - center) / radius;
+            return true;
+        }
+
+        temp = (-b + sqrt(b * b - a * c)) / a;
+        if (temp < tMax && temp > tMin)
+        {
+            rec.t = temp;
+            rec.p = r.pointAtParameter(rec.t);
+            rec.normal = (rec.p - center) / radius;
+            return true;
+        }
+    }
+
+    return false;
+}
+
+#endif
